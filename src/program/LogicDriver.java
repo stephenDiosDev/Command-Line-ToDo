b3049c9b8af6e4bef5e5cd45cf9d97fde3f2c295
Stephen Dios <stephendiosdeveloper@gmail.com>
2020-09-22 16:15:35 -0600

    Created empty commands and tested GUI calls them correctly

--- src/program/LogicDriver.java
+++ src/program/LogicDriver.java
@@ -1,10 +1,76 @@
 package program;
 
+import storage.Storage;
+
 //contains the actual program logic, takes inputs from ProgramGUI and sends strings as outputs
 public class LogicDriver {
     private FileIO fileIO;
 
     public LogicDriver() {
         fileIO = new FileIO();
+    }
+
+    public void newList() {
+        System.out.println("\nNEW LIST CREATED\n");
+        Storage.items.clear(); // empty previous items
+    }
+
+    // save whats in storage into file given a file name
+    public void saveList(String filename) {
+        System.out.println("\nSAVING LIST NAMED: " + filename + "\n");
+    }
+
+    // load items from filename into storage
+    public void loadList(String filename) {
+        System.out.println("\nLOADING LIST NAMED: " + filename + "\n");
+    }
+
+    public void removeList(String filename) {
+        System.out.println("\nREMOVING LIST NAMED: " + filename + "\n");
+    }
+
+    public void newGenericItem(String name) {
+        System.out.println("CREATING NEW GENERIC ITEM WITH NAME: " + name + "\n");
+    }
+
+    public void newDeadlineItem(String name, String desc, String date) {
+        System.out.println(
+                "CREATING NEW DEADLINE ITEM WITH NAME: " + name + "\nAND DESC: " + desc + "\nAND DATE: " + date + "\n");
+    }
+
+    public void newHabitItem(String name, String desc, int recurring) {
+        System.out.println("CREATING NEW HABIT ITEM WITH NAME: " + name + "\nAND DESC: " + desc + "\nAND RECURRING: "
+                + recurring + "\n");
+    }
+
+    public void newWorkItem(String name, String desc) {
+        System.out.println("CREATING NEW WORK ITEM WITH NAME: " + name + "\nAND DESC: " + desc + "\n");
+    }
+
+    // given an item name, load from storage, edit changes and store new version in
+    // storage
+    public void editItem(String name) {
+        System.out.println("EDITING ITEM WITH NAME: " + name + "\n");
+    }
+
+    // given item name, remove it from storage
+    public void removeItem(String name) {
+        System.out.println("REMOVING ITEM WITH NAME: " + name + "\n");
+    }
+
+    // given item name, check the status to true
+    public void completeItem(String name) {
+        System.out.println("COMPLETING ITEM WITH NAME: " + name + "\n");
+    }
+
+    // given item name, uncheck the status to false
+    public void revertItem(String name) {
+        System.out.println("REVERTING ITEM WITH NAME: " + name + "\n");
+    }
+
+    // given item name, check storage to ensure there is no other item with the same
+    // name
+    private void duplicateNameCheck(String name) {
+
     }
 }
--- src/program/ProgramGUI.java
+++ src/program/ProgramGUI.java
@@ -19,8 +19,48 @@ public class ProgramGUI {
         while (!input.contains("exit")) { // exit is the escape word
             if (input.contains("clear")) {
                 clearScreen();
-            } else if (input.contains("help")) {
+            } else if (input.contains("help") && input.indexOf("help") == 0) {
                 availableCommands();
+            } else if (input.contains("new list") && input.indexOf("new list") == 0) {
+                driver.newList();
+            } else if (input.contains("save") && input.indexOf("save") == 0) {
+                driver.saveList(input.substring(input.indexOf("save") + 4));
+            } else if (input.contains("load") && input.indexOf("load") == 0) {
+                driver.loadList(input.substring(input.indexOf("load") + 4));
+            } else if (input.contains("remove list") && input.indexOf("remove list") == 0) {
+                driver.removeList(input.substring(input.indexOf("remove list") + 11));
+            } else if (input.contains("new generic") && input.indexOf("new generic") == 0) {
+                driver.newGenericItem(input.substring(input.indexOf("new generic") + 11));
+            } else if (input.contains("new deadline") && input.indexOf("new deadline") == 0) {
+                String desc, date;
+                System.out.println("Please enter the deadline date (yyyy-mm-dd)");
+                date = in.nextLine();
+                System.out.println("Please enter a short description");
+                desc = in.nextLine();
+                driver.newDeadlineItem(input.substring(input.indexOf("new deadline") + 12), desc, date);
+            } else if (input.contains("new habit") && input.indexOf("new habit") == 0) {
+                String desc;
+                int days;
+                System.out.println("Please enter the space between days (0 = everyday, 1 = every other day)");
+                days = Integer.parseInt(in.nextLine());
+                System.out.println("Please enter a short description");
+                desc = in.nextLine();
+                driver.newHabitItem(input.substring(input.indexOf("new habit") + 9), desc, days);
+            } else if (input.contains("new work") && input.indexOf("new work") == 0) {
+                String desc;
+                System.out.println("Please enter a short description");
+                desc = in.nextLine();
+                driver.newWorkItem(input.substring(input.indexOf("new work") + 8), desc);
+            } else if (input.contains("edit") && input.indexOf("edit") == 0) {
+                driver.editItem(input.substring(input.indexOf("edit") + 4));
+            } else if (input.contains("remove") && input.indexOf("remove") == 0) {
+                driver.removeItem(input.substring(input.indexOf("remove") + 6));
+            } else if (input.contains("complete") && input.indexOf("complete") == 0) {
+                driver.completeItem(input.substring(input.indexOf("complete") + 8));
+            } else if (input.contains("uncomplete") && input.indexOf("uncomplete") == 0) {
+                driver.revertItem(input.substring(input.indexOf("uncomplete") + 10));
+            } else {
+                System.out.println("\nYour command \"" + input + "\" is not a recognized command!");
             }
 
             input = in.nextLine();
@@ -50,16 +90,32 @@ public class ProgramGUI {
 
     public void startUpScreen() {
         clearScreen();
-        clearScreen();
-        System.out.println("Welcome to S^3 Habit and To Do List Program!");
-        availableCommands();
+        System.out.println("Welcome to the Habit and To Do List Program!\n");
+        System.out.println("To see a list of every available command, type help\n");
     }
 
     public void availableCommands() {
         System.out.println("Available Commands: (please keep one command per line!)");
-        System.out.println("    Exit: Closes the program");
-        System.out.println("    New List: Creates a new to do list");
-        System.out.println("    Save listname: Saves the current to do list under with the name listname");
-        System.out.println("    Help: Lists the available commands");
+        System.out.println("    exit: Closes the program");
+        System.out.println("    new list: Creates a new to do list");
+        System.out.println("    save listname: Saves the current to do list with the name listname");
+        System.out.println("    load listname: Loads the list named listname");
+        System.out.println("    remove list listname: Deletes the list named listname");
+        System.out.println(
+                "    new generic itemname: Creates a new generic item named itemname, and adds it to the current list");
+        System.out.println(
+                "    new deadline itemname: Creates a new deadline item named itemname, and adds it to the current list");
+        System.out.println(
+                "    new habit itemname: Creates a new habit item named itemname, and adds it to the current list");
+        System.out.println(
+                "    new work itemname: Creates a new work item named itemname, and adds it to the current list");
+        System.out.println("    edit itemname: Edits the item named itemname from the current list");
+        System.out.println("    remove itemname: Deletes the item named itemname from the current list");
+        System.out.println("    complete itemname: Checks the item named itemname as complete from the current list");
+        System.out.println(
+                "    uncomplete itemname: Unchecks the item named itemname as uncomplete from the current list");
+        System.out.println("    help: Lists the available commands\n");
+        System.out
+                .println("    ********PLEASE ENTER COMMANDS IN ALL LOWER CASE (file names can use capitals)********\n");
     }
 }
